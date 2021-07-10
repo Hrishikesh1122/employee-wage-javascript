b8f3369b8b2ff2abe392e79a28d64d3a05424ce2
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-07-10 23:06:10 +0530

    added : uc4 ability to calculate monthly wage

--- empWageFunctions.js
+++ empWageFunctions.js
@@ -1,15 +1,36 @@
+const WAGE_PER_HR =20;
+const NUMBER_OF_WORKING_DAYS = 20;
+
 //UC2 Ability to calucate daily wage of a employee.
 function calcDailyEmpWage(){
     var partTime = document.getElementById("parttime");
     var fullTime = document.getElementById("fulltime");
-    var wagePerHr = 20;
     var dailyWage =0;
     if(partTime.checked)
-        dailyWage=wagePerHr*(partTime.value)
+        {dailyWage=WAGE_PER_HR*(partTime.value)}
     else if (fullTime.checked)        
-        dailyWage = wagePerHr*(fulltime.value)
-    else
+        {dailyWage = WAGE_PER_HR*(fulltime.value)}
+    else {
         alert("Please select type of employee")
+        return
+    }
 
     alert("Wage of employee is : "+dailyWage)
+}
+
+//UC4 Calculate Monthly wage assuming 20 hrs a day
+function calcMonthlyWage(){
+    var partTime = document.getElementById("parttime");
+    var fullTime = document.getElementById("fulltime");
+    var monthlyWage =0;
+    if(partTime.checked)
+        {monthlyWage=WAGE_PER_HR*(partTime.value)*NUMBER_OF_WORKING_DAYS}
+    else if (fullTime.checked)        
+        {monthlyWage = WAGE_PER_HR*(fulltime.value)*NUMBER_OF_WORKING_DAYS}
+    else {
+        alert("Please select type of employee")
+        return
+    }
+
+    alert("Monthly Wage of employee is : "+monthlyWage)
 }
